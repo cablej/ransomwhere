cb084695c846457a0b89bc6c64cdf2a671ad3e83
Jack Cable <jackhcable@gmail.com>
2021-06-14 00:23:49 -0500

    Update license

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,7 @@
 /*
-	Phantom by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Phantom by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
 */
 
 var ransomTotal = 2.51;
